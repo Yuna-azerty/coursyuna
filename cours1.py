de8b9e28d05b18e60e8e149a23961818c041ba9c
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-10-29 19:13:53 +0100

    cours + devoir

---
+++ cours1.py
@@ -0,0 +1,25 @@
+#for i in range(10,0,-1):
+#    print(i)
+#print("boom")   
+
+#nombreATrouver=1
+#nombreRentre=0
+#while nombreRentre!=nombreATrouver:
+#    nombreRentre = int(input("Donner un nombre : "))
+    
+# #nombreMax = int(input("Donner un nombre :"))
+# compteur = 1
+# while compteur!=nombreMax:
+#     print(compteur)
+#     compteur +=1
+    
+# nombreChoisi =  int(input("Donner un nombre : "))
+# for a in range(1, nombreChoisi+1):
+#     print(a)
+    
+#Jeux : Trouver le nombre entre 1 et 20 
+    #Affecter un nombre entre 1 et 20 à une variable
+    #demander un nombre : si plus petit que celui à trouver, 
+    # écrire "plus petit" sinon écrire "plus grand" et si le nombre est égal écrire "trouvé !"
+    
+    
